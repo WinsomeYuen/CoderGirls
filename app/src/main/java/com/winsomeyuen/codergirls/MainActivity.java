722a30eb4a3aaa305fbc19afe8fc71ffc0a234ea
WinsomeYuen <winsomeyuen@hotmail.com>
2017-11-08 23:21:29 +0000

    "second commit"

--- app/src/main/java/com/winsomeyuen/codergirls/MainActivity.java
+++ app/src/main/java/com/winsomeyuen/codergirls/MainActivity.java
@@ -1,19 +1,20 @@
 package com.winsomeyuen.codergirls;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
-        android.support.v7.app.AppCompatActivity actionBar;
-        getSupportActionBar().setTitle("CoderGirls");
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.icon);
-        getSupportActionBar().setIcon(R.drawable.icon);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
